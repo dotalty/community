f347eeea6ecb78d892830788e6cfb61949a9437f
dotalty <291915054@qq.com>
2020-04-13 12:21:37 +0800

    add list questions

--- src/main/java/com/atschool/study/community/service/QuestionService.java
+++ src/main/java/com/atschool/study/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.atschool.study.community.service;
 
+import com.atschool.study.community.dto.PaginationDTO;
 import com.atschool.study.community.dto.QuestionDTO;
 import com.atschool.study.community.mapper.QuestionMapper;
 import com.atschool.study.community.mapper.UserMapper;
@@ -21,9 +22,21 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question>questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if(page < 1){
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+        Integer offset = size * (page - 1);
+        List<Question>questions = questionMapper.list(offset,size);
         List<QuestionDTO>questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -31,6 +44,8 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
