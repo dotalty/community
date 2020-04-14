cca7b14ea32dd078a1dfbdede09186e4dafa6295
dotalty <291915054@qq.com>
2020-04-14 15:49:46 +0800

    personal questions list

--- src/main/java/com/atschool/study/community/mapper/QuestionMapper.java
+++ src/main/java/com/atschool/study/community/mapper/QuestionMapper.java
@@ -6,8 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
-import sun.awt.SunHints;
+
 
 import java.util.List;
 
@@ -22,4 +21,10 @@ public interface QuestionMapper {
 
     @Select("select count(1) from question")
     Integer count();
+
+    @Select("select * from question where creator = #{userId} limit #{offset},#{size}")
+    List<Question> listByUserId(@Param(value = "userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
+
+    @Select("select count(1) from question where creator = #{userId}")
+    Integer countByUserId(@Param(value = "userId") Integer userId);
 }
